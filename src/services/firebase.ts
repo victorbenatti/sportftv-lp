bb9f3bb0b25f1515b78761c63c0587d3b0f783b4
Victor Benatti <victorbenatti13@gmail.com>
2025-08-07 16:31:15 -0300

    firebase: adiciona configuração do Firebase\n\n- Cria arquivo de configuração do Firebase\n- Inicializa Firestore para armazenamento de dados\n- Configura conexão com projeto Firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+// src/services/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Suas credenciais do Firebase que você pega no console do projeto
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
